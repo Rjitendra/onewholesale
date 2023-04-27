ee425812af387497c2ab3512711c52a967a7200a
Jitendra Behera <jb.jitu@live.com>
2023-04-27 11:32:33 +0530

    Initial Project Setup

---
+++ OneWholeSale.Client/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OneWholeSale.Client.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ OneWholeSale.Service/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace OneWholeSale.Service
+{
+    public class Class1
+    {
+
+    }
+}
