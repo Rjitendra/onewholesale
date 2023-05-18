98f8018a02653940752d8bb8a4507f66035d135b
Jitendra Behera <jb.jitu@live.com>
2023-05-18 20:03:45 +0530

    changes

--- OneWholeSale.Service/Implementations/SalesPersonService.cs
+++ OneWholeSale.Service/Implementations/SalesPersonService.cs
@@ -4,29 +4,34 @@
     using Microsoft.AspNetCore.Http;
     using Microsoft.EntityFrameworkCore;
     using OneWholeSale.Model.Context;
+    using OneWholeSale.Model.Dto;
     using OneWholeSale.Model.Dto.SalesPerson;
     using OneWholeSale.Model.Entity.SalesPerson;
+    using OneWholeSale.Model.Enums;
     using OneWholeSale.Service.Interfaces;
     using OneWholeSale.Service.Utility;
-    using System.Net.Sockets;
     using System.Threading.Tasks;
 
     public class SalesPersonService : ISalesPersonService
     {
         private ApiContext Db { get; set; }
 
-        private readonly IHttpContextAccessor _httpContextAccessor;
-        private IMapper _mapper;
 
+        private IMapper _mapper;
+        private CurrentUser CurrentUser { get; set; }
+        private IUserService UserService { get; }
         public SalesPersonService(
              ApiContext db,
-            IHttpContextAccessor httpContextAccessor,
+             IHttpContextAccessor context,
+             IUserService userService,
              IMapper mapper
            )
         {
             this.Db = db;
             _mapper = mapper;
-            _httpContextAccessor = httpContextAccessor;
+            UserService = userService;
+
+            this.CurrentUser = new CurrentUser(context.HttpContext.User);
         }
         public async Task<Result<SalesPersonDto>> GetSalesPerson(int id)
         {
@@ -41,19 +46,78 @@
         }
         public async Task<Result<bool>> AddSalesPerson(SalesPersonDto dto)
         {
-            try
+            using (var transaction = this.Db.Database.BeginTransaction())
             {
-                var result = _mapper.Map<SalesPerson>(dto);
-                // Add the ticket to the DbContext's Ticket DbSet.
-                await this.Db.SalesPerson.AddAsync(result);
+                try
+                {
+                    // get all roles
+                    var roleList = await this.UserService.Roles();
 
-                // Save the changes to the database.
-                await this.Db.SaveChangesAsync();
+                    // Get the role ID for "Tenant" role
+                    var roleId = roleList.Entity
+                                   .Where(x => x.Name == ApplicationUserRole.SalesPerson.ToString())
+                                   .Select(x => x.Id)
+                                   .FirstOrDefault();
 
-                // Return a Result object indicating success and a value of true.
-                return Result<bool>.Success(true);
+
+
+                    // Create a list of ApplicationUserDto objects
+                    var users = new ApplicationUserDto()
+                    {
+                        FirstName = dto.Name,
+                        LastName = "",
+                        UserName = dto.Email,
+                        Email = dto.Email,
+                        Password = dto.Email + "-Sale64#",
+                        IsEnabled = true,
+                        UserRoles = new List<ApplicationUserRoleDto>()
+                                           {
+                                                new ApplicationUserRoleDto(){ Id = roleId, Name = ApplicationUserRole.SalesPerson.ToString() }
+                                           }
+                    };
+
+                    // Create the users using UserService.CreateUser method
+                    var userResponse = this.UserService.CreateUser(users);
+
+                    if (userResponse <= 0)
+                    {
+                        return Result<bool>.Failure("Failed to create Sales Person");
+                    }
+
+                    var applicationUser = await this.Db.Users
+                                                   .Where(a => a.UserName == users.UserName || a.Email == users.Email).SingleOrDefaultAsync();
+
+
+                    var result = _mapper.Map<SalesPerson>(dto);
+                    result.UserId = applicationUser.Id;
+
+                    // Add the SalesPerson to the DbContext's SalesPerson DbSet.
+                    await this.Db.SalesPerson.AddAsync(result);
+
+                    // Save the changes to the database.
+                    await this.Db.SaveChangesAsync();
+
+                    transaction.Commit();
+                    // Return a Result object indicating success and a value of true.
+                    return Result<bool>.Success(true);
+                }
+
+                catch (Exception ex)
+                {
+
+                    transaction.Rollback();
+
+                    var applicationUser = await this.Db.Users
+                                                    .Where(a => a.UserName == dto.Email || a.Email == dto.Email).SingleOrDefaultAsync();
+                    if (applicationUser != null)
+                    {
+                        bool res = UserService.DeleteApplicationUser(applicationUser.Id);
+
+                        return Result<bool>.Failure("Failed to create Sales Person");
+                    }
+                    return Result<bool>.Failure("Failed to create Sales Person");
+                }
             }
-            catch (Exception ex) { return Result<bool>.Failure("Error in Adding Sales Person"); }
         }
 
         public async Task<Result<bool>> DeleteSalesPerson(int id)
@@ -79,8 +143,8 @@
             try
             {
                 var salesPerson = _mapper.Map<SalesPerson>(dto);
-                
-               
+
+
                 await this.Db.SaveChangesAsync();
                 return Result<bool>.Success(true);
             }
---
+++ OneWholeSale.Service/Implementations/UserService.cs
@@ -0,0 +1,207 @@
+﻿namespace OneWholeSale.Service.Implementations
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+    using OneWholeSale.Model.Context;
+    using OneWholeSale.Model.Dto;
+    using OneWholeSale.Model.Entity;
+    using OneWholeSale.Model.Enums;
+    using OneWholeSale.Service.Interfaces;
+    using OneWholeSale.Service.Utility;
+    using System.Linq;
+
+    public class UserService : IUserService
+    {
+        public UserService(ApiContext db, IHttpContextAccessor context)
+
+        {
+            this.Db = db;
+            this.CurrentUser = new CurrentUser(context.HttpContext.User);
+        }
+
+        /// <summary>
+        /// Current user.
+        /// </summary>
+        private CurrentUser CurrentUser { get; set; }
+
+        private ApiContext Db { get; set; }
+
+        public int GetUserId()
+        {
+            return CurrentUser.ApplicationUserId;
+        }
+        public int CreateUser(ApplicationUserDto entity)
+        {
+            try
+            {
+                var userExisted = this.Db.Users.Where(a => a.UserName == entity.UserName || a.Email == entity.Email).SingleOrDefault();
+
+                if (userExisted != null)
+                {
+                    // if Username is duplicated returns -1 and its handled in Angualr side to display error message
+                    return -1;
+                }
+
+                var appUser = new ApplicationUser(entity);
+                this.Db.Users.Add(appUser);
+                this.Db.SaveChanges();
+
+                if (appUser.Id > 0)
+                {
+                    this.AddUserToTheRole(entity, appUser.Id);
+
+                    // Adding user information into the claims
+                    this.AddOrUpdateUserClaim(IdentityClaims.FullName, $"{entity.FirstName} {entity.LastName}", appUser.Id);
+                    this.AddOrUpdateUserClaim(IdentityClaims.FirstName, entity.FirstName, appUser.Id);
+                    this.AddOrUpdateUserClaim(IdentityClaims.LastName, entity.LastName, appUser.Id);
+                    this.AddOrUpdateUserClaim(IdentityClaims.Email, entity.Email, appUser.Id);
+
+                    return appUser.Id;
+                }
+                else
+                {
+                    // if user info is not saved and its handled in Angualr side to display error message
+                    return 0;
+                }
+            }
+            catch (Exception ex) { throw ex; }
+        }
+
+        public int CreateMultipleUser(IList<ApplicationUserDto> dto)
+        {
+            try
+            {
+                var userExisted = this.Db.Users.Where(a => (dto.Select(x => x.Email).ToList()).Contains(a.Email)).AsNoTracking().ToList();
+                if (userExisted.Count() != 0) { return -1; }
+                int id = 0;
+                foreach (var entity in dto)
+                {
+                    var result = CreateUser(entity);
+
+                    id = result;
+                }
+
+                return id;
+            }
+            catch (Exception ex) { return -1; }
+        }
+
+        public int UpdateUser(ApplicationUserDto entity)
+        {
+            var user = this.Db.Users.Find(entity.ApplicationUserId);
+            if (user == null)
+            {
+                return 0;
+            }
+            // base requirement need  to modify
+            user.Update(entity);
+            this.Db.SaveChanges();
+
+            return user.Id;
+        }
+
+        public bool DeleteApplicationUsers(List<int> ids)
+        {
+            try
+            {
+                foreach (var id in ids)
+                {
+                    var user = this.Db.Users.Where(x => x.Id == id).SingleOrDefault();
+                    this.Db.Users.Remove(user);
+                    this.Db.SaveChanges();
+                };
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // log the exception here
+                return false;
+            }
+        }
+
+        public bool DeleteApplicationUser(int id)
+        {
+            try
+            {
+                var user = this.Db.Users.Where(x => x.Id == id).SingleOrDefault();
+                this.Db.Users.Remove(user);
+                this.Db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // log the exception here
+                return false;
+            }
+        }
+
+        public async Task<Result<IEnumerable<ApplicationUserRoleDto>>> Roles()
+        {
+            try
+            {
+                var UserRoles = await this.Db.Roles.Select(a => new ApplicationUserRoleDto { Id = (ApplicationUserRole)a.Id, Name = a.Name }).ToListAsync();
+                return Result<IEnumerable<ApplicationUserRoleDto>>.Success(UserRoles);
+            }
+            catch (Exception ex) { throw ex; }
+        }
+
+        /// <summary>
+        /// Method for Adding user into the role
+        /// </summary> 
+        /// <param name="entity"></param>
+        /// <param name="userId"></param>
+        private void AddUserToTheRole(ApplicationUserDto entity, int userId)
+        {
+            try
+            {
+                var userRole = new List<IdentityUserRole<int>>();
+
+                // Create role(s) for user
+                foreach (var role in entity.UserRoles)
+                {
+                    userRole.Add(new IdentityUserRole<int>()
+                    {
+                        RoleId = (int)role.Id,
+                        UserId = userId
+                    });
+                }
+
+                this.Db.UserRoles.AddRange(userRole);
+                this.Db.SaveChanges();
+            }
+            catch (Exception ex) { throw ex; }
+        }
+
+        /// <summary>
+        /// Adds or updates the specified user claim type for the specified user.
+        /// </summary>
+        /// <param name="claimType">Claim type.</param>
+        /// <param name="claimValue">Claim value.</param>
+        /// <param name="applicationUserId">User whose claims are to be modified.</param>
+        /// <returns></returns>
+
+        private void AddOrUpdateUserClaim(string claimType, string claimValue, int applicationUserId)
+        {
+            try
+            {
+                // fetch existing claim
+                var claim = this.Db.UserClaims.SingleOrDefault(x => x.ClaimType == claimType && x.UserId == applicationUserId);
+
+                // if no claim exists, create one
+                if (claim == null)
+                {
+                    this.Db.UserClaims.Add(new IdentityUserClaim<int> { ClaimType = claimType, ClaimValue = claimValue, UserId = applicationUserId });
+                    this.Db.SaveChanges();
+                }
+                else
+                {
+                    // otherwise, update the claim
+                    claim.ClaimValue = claimValue;
+                }
+            }
+            catch (Exception ex) { throw ex; }
+        }
+    }
+}
